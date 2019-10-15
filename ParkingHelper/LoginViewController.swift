ce98abf92a0ca8cca71c29e5d75f7b7491d4d65b
Pranay Boggarapu <pranayboggarapu@gmail.com>
2019-10-14 19:16:42 -0700

    Base Structure of the project

---
+++ ParkingHelper/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParkingHelper
+//
+//  Created by Pranay Boggarapu on 9/23/19.
+//  Copyright © 2019 Pranay Boggarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
